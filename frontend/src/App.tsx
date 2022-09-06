34f5ff74bfde65692c2d638b7705f3bcb37f594d
Joao Paulo <jpaulo1993@live.com>
2022-09-06 00:00:33 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
-   return(
-    <h1>Olá mundo!</h1>
-   )
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  )
 
 }
 
