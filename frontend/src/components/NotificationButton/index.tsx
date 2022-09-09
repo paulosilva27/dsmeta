01c878f3a6ba46f2a9b8f268443d01b3c903903c
Joao Paulo <jpaulo1993@live.com>
2022-09-09 13:19:01 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,6 +2,7 @@ import icon from '../../assets/img/notification_icon.svg';
 import  "./styles.css";
 import axios from 'axios';
 import { BASE_URL } from '../../utils/request';
+import { toast } from 'react-toastify';
 
 type Props = {
      saleId: number;
@@ -10,7 +11,7 @@ type Props = {
 function handleClick(id: number){
     axios(`${BASE_URL}/sales/${id}/notification`)
         .then(response => {
-            console.log("SUCESSO");
+            toast.info("SMS enviado com sucesso");
         })
 }
 
